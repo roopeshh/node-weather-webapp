4477264c9f48bae07a82def09f47be31a440e06b
Roopesh Hiriyanna <roopesh.hiriyanna@dazn.com>
2019-05-17 15:55:31 +0200

    Changes to setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicDirectory = path.join(__dirname, '../public');
 const viewsDirectory = path.join(__dirname, '../templates/views')
@@ -82,6 +83,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server started at port 3000')
+app.listen(port, () => {
+    console.log('Server started at port:', port);
 });
